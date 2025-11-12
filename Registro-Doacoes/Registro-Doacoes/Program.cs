c611b5ee5a1e086565b0715d8ea2ab8a48fd0ac3
nmediinaa <nicolascostam23@gmail.com>
2025-11-12 18:10:58 -0300

    :sparkles: feat: Exercicio registro-doacao

---
+++ Registro-Doacoes/Registro-Doacoes/Program.cs
@@ -0,0 +1,21 @@
+﻿//Exercicio 2 - Alura
+
+Console.WriteLine("por gentileza, quanto gostaria de doar?");
+double valorDoacao = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Gostaria de efetuar a doação anonima? [S/N]");
+char anonima = char.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Conta corrente ou poupança?[C/P]");
+char tipoDeConta = char.Parse(Console.ReadLine()!);
+
+    
+if (anonima == 'S')
+{
+    bool controleAnonima = true;
+    Console.WriteLine($"Valor da doação: {valorDoacao}\n Doação anonima: {controleAnonima}\n Tipo de conta: {tipoDeConta}");
+}else if (anonima == 'N')
+{
+    bool controleAnonima = false;
+    Console.WriteLine($"Valor da doação: {valorDoacao}\n Doação anonima: {controleAnonima}\n Tipo de conta: {tipoDeConta}");
+}
