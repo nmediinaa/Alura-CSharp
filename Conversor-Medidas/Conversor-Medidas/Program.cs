b774d3725c786151751e8377d4bf0e3e9c92a12d
nmediinaa <nicolascostam23@gmail.com>
2025-11-13 09:18:50 -0300

    :sparkles: feat: Exercicio 3 - Convertendo distancias

---
+++ Conversor-Medidas/Conversor-Medidas/Program.cs
@@ -0,0 +1,8 @@
+﻿const double km = 1.60934;
+
+Console.Write("Quantas milhas a viagem tem?\n");
+double milhas =  double.Parse(Console.ReadLine()!);
+
+double medidaMétrica = milhas*km;
+
+Console.Write($"{milhas} equivalem a {medidaMétrica} quilômetros");
