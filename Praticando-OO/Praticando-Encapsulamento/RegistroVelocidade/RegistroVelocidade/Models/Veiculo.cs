8892919a196f058d8b0c0d2c5613d27a7507e412
nmediinaa <nicolascostam23@gmail.com>
2025-12-01 23:03:03 -0300

    :sparkles: feat: Add first exercise from encapsulation module

---
+++ Praticando-OO/Praticando-Encapsulamento/RegistroVelocidade/RegistroVelocidade/Models/Veiculo.cs
@@ -0,0 +1,26 @@
+﻿namespace RegistroVelocidade.Models;
+
+public class Veiculo
+{
+    
+    public string Placa { get; set; }
+    private int _velocidade;
+    
+
+
+    public Veiculo(string placa)
+    {
+        this.Placa = placa;
+    }
+    
+    
+    public void AtualizarVelocidade(int valor)
+    {
+        if (valor > 0) this._velocidade = valor;
+    }
+
+    public int VelocidadeAtual
+    {
+        get{return this._velocidade;}
+    }
+}
---
+++ Praticando-OO/Praticando-Encapsulamento/RegistroVelocidade/RegistroVelocidade/Program.cs
@@ -0,0 +1,7 @@
+﻿using RegistroVelocidade.Models;
+
+Veiculo carro = new Veiculo("ABC-0909");
+carro.AtualizarVelocidade(70);
+
+Console.WriteLine("Veículo: " + carro.Placa);
+Console.WriteLine("Velocidade atual: " + carro.VelocidadeAtual + " km/h");
