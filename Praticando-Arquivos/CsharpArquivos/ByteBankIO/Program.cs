f4b1f1ccdc1f5e8e1b21907b0bae90c6817e01cf
nmediinaa <nicolascostam23@gmail.com>
2025-12-11 14:48:55 -0300

    :sparkles: feat: Add a ReadBuffer function and a loop to read files

---
+++ Praticando-Arquivos/CsharpArquivos/ByteBankIO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using ByteBankIO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string enderecoDoArquivo = "contas.txt";
+        FileStream fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);     
+        
+        var buffer = new byte[1024]; //1 KB
+
+        int numerosDeBytesLidos = -1; 
+        while (numerosDeBytesLidos != 0)
+        {
+            numerosDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            ReadBuffer(buffer);
+        }
+    }
+
+
+    static void ReadBuffer(byte[] buffer)
+    {
+        UTF8Encoding utf8 = new();
+        string text = utf8.GetString(buffer);
+        Console.Write(text);
+    }
+    
+}
