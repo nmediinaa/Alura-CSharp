08d9d5c94e0cc9f5d9dfc1477af86c15a67902e1
nmediinaa <nicolascostam23@gmail.com>
2025-12-11 18:56:11 -0300

    :sparkles: feat:Add using for more security with files

--- Praticando-Arquivos/ByteBankIO/ByteBankIO/Program.cs
+++ Praticando-Arquivos/ByteBankIO/ByteBankIO/Program.cs
@@ -5,16 +5,22 @@ class Program
     static void Main(string[] args)
     {
         string enderecoDoArquivo = "contas.txt";
-        FileStream fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);     
-        
-        var buffer = new byte[1024]; //1 KB
 
-        int numerosDeBytesLidos = -1; 
-        while (numerosDeBytesLidos != 0)
+        //Método para proteger contra excessões e evita vazamento de memória e bloqueio de arquivos
+        using (FileStream fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
         {
-            numerosDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-            ReadBuffer(buffer);
+            var buffer = new byte[1024]; //1 KB
+
+            int numerosDeBytesLidos = -1; 
+            while (numerosDeBytesLidos != 0)
+            {
+                numerosDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                ReadBuffer(buffer);
+            }
+            
+            fluxoDoArquivo.Close();
         }
+        
     }
 
 
