0adf640590f4ad667c6173a6073e990e9baaac39
nmediinaa <nicolascostam23@gmail.com>
2025-12-13 20:32:11 -0300

    :recycle: refactor: use StreamReader for file manipulation

--- Praticando-Arquivos/ByteBankIO/ByteBankIO/LeitorDeArquivo.cs
+++ Praticando-Arquivos/ByteBankIO/ByteBankIO/LeitorDeArquivo.cs
@@ -28,5 +28,5 @@ public class LeitorDeArquivo
             Console.Write(text);
         }
     }
-
+ 
 }
--- Praticando-Arquivos/ByteBankIO/ByteBankIO/Program.cs
+++ Praticando-Arquivos/ByteBankIO/ByteBankIO/Program.cs
@@ -4,7 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        LeitorDeArquivo.ReadFile("contas.txt");
+        string filePath = "contas.txt";
+        
+        //LeitorDeArquivo.ReadFile(filePath);
+
+
+        using (FileStream fluxoDoArquivo = new FileStream(filePath, FileMode.Open))
+        {
+            StreamReader reader = new StreamReader(fluxoDoArquivo);
+
+            //Console.WriteLine(reader.ReadLine()); --> Le a primeira linha
+            //Console.WriteLine(reader.ReadToEnd()); --> Carrega o arquivo inteiro na memória(Não aconselhavel) 
+            //Console.WriteLine(reader.Read()); --> Retorna em Byte o PRIMEIRO caracter do arquivo
+            
+            //Aqui o processe nn carrega o arquivo inteiro na memória sendo muito parecido com o primeiro processo
+            while (!reader.EndOfStream)//Le até o fim do arquivo
+            {
+                Console.WriteLine(reader.ReadLine());
+            }
+        }    
+        
+        
+        
+        
+        
     }
 
 }
