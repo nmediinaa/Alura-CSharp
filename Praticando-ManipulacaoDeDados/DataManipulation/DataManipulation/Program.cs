462831438cf7cd2922e0cb6063f1905dee7fd81b
nmediinaa <nicolascostam23@gmail.com>
2025-12-22 19:42:07 -0300

    :sparkles: feat: Add a new module to practise data manipulation and LINQ

---
+++ Praticando-ManipulacaoDeDados/DataManipulation/DataManipulation/Program.cs
@@ -0,0 +1,25 @@
+﻿//var diasDaSemana = new ArrayList(){"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"};
+
+
+var carrinho = new List<Produto>()
+{
+    new Produto(){Nome = "Livro", Preco = 49.90},
+    new Produto(){Nome = "Garrafa", Preco = 10.50}
+};
+
+
+foreach (Produto p in carrinho)
+{
+    Console.WriteLine(p.Nome + ": R$" + p.Preco);
+}
+
+class Produto()
+{
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+}
+
+class DiasDaSemana()
+{
+    private
+}
