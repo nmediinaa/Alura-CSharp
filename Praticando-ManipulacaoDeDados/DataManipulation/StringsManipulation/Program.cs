1f81222155e4ba06d05528c85a3209ffe868f179
nmediinaa <nicolascostam23@gmail.com>
2026-01-05 17:00:43 -0300

    :recycle: refactor: Add a data validation on MusicasDoCsv method

--- Praticando-ManipulacaoDeDados/DataManipulation/StringsManipulation/Program.cs
+++ Praticando-ManipulacaoDeDados/DataManipulation/StringsManipulation/Program.cs
@@ -1,7 +1,10 @@
 ﻿using var stream = new FileStream("musicas.csv", FileMode.Open);
 using var reader = new StreamReader(stream);
 
-var musicas = MusicasDoCsv(reader);
+var musicas = MusicasDoCsv(reader)
+    .Where(m => m.Artista.Equals("Coldplay", StringComparison.OrdinalIgnoreCase))
+    .Take(5);
+
 ExibirMusicas(musicas);
 //Métodos
 void ExibirMusicas(IEnumerable<Musica> musicas)
@@ -15,7 +18,7 @@ void ExibirMusicas(IEnumerable<Musica> musicas)
     Console.WriteLine("Musicas do Arquivo".PadLeft(53));
     Console.WriteLine(colunaTitulo + colunaArtista + colunaDuracao + colunaGenero + colunaLancamento);
     Console.WriteLine("".PadRight(120, '='));
-    
+    int cont = 0;
     foreach (var musica in musicas.Take(5))
     {
         Console.WriteLine($"{musica.Titulo,-40}{musica.Artista,-20}{musica.Duracao/60.0,-20:F2}{musica.Generos[0],-20}{musica.Lancamento,-20:dd/MM/yyyy}");
@@ -48,16 +51,18 @@ IEnumerable<Musica> MusicasDoCsv(StreamReader leitor)
     while (linha != null)
     {
         var partes = linha.Split(';');
-        //if (partes.Length != 4) continue;
-        var musica = new Musica
+        if (partes.Length == 5)
         {
-            Titulo = partes[0],
-            Artista = partes[1],
-            Duracao = Convert.ToInt32(partes[2]),
-            Generos = partes[3].Split(',', StringSplitOptions.TrimEntries),//remove os espaços em branco indesejados
-            Lancamento = Convert.ToDateTime(partes[4])
-        };
-        yield return musica;
+            var musica = new Musica
+            {
+                Titulo = string.IsNullOrWhiteSpace(partes[0]) ?  "Titulo não encontrado" : partes[0], 
+                Artista = string.IsNullOrEmpty(partes[1]) ? "Artista não encontrado" : partes[1],
+                Duracao = int.TryParse(partes[2], out int duracao) ? duracao : 0,
+                Generos = partes[3].Split(',', StringSplitOptions.TrimEntries),//remove os espaços em branco indesejados
+                Lancamento = DateTime.TryParse(partes[4], out DateTime data) ? data : DateTime.Now
+            };   
+            yield return musica;
+        }
         linha = leitor.ReadLine();
     }
 }
