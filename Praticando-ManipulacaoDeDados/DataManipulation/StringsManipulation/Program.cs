b9cf8e75a408b3310158668f7b4a46f839b2b6b4
nmediinaa <nicolascostam23@gmail.com>
2026-01-05 14:57:23 -0300

    :sparkles: feat: Add a new method to diplay all music information with string format

--- Praticando-ManipulacaoDeDados/DataManipulation/StringsManipulation/Program.cs
+++ Praticando-ManipulacaoDeDados/DataManipulation/StringsManipulation/Program.cs
@@ -1,21 +1,46 @@
 ﻿using var stream = new FileStream("musicas.csv", FileMode.Open);
 using var reader = new StreamReader(stream);
 
-var senha = "Nicolas@26";
-
-int totalCaracteres = senha.Length;// ou poderiamos usar o .Count()
-int possuiLetrasMaiusculas = senha.Count(c => char.IsUpper(c)!);
-int possuiLetrasMinusculas = senha.Count(c => char.IsLower(c)!);
-int possuiNumero = senha.Count(c => char.IsDigit(c)!);
-int possuiSimbolo = senha.Count(c => !char.IsLetterOrDigit(c));
-
-if (totalCaracteres < 8 || possuiLetrasMaiusculas == 0 ||
-    possuiLetrasMinusculas == 0 || possuiNumero == 0 ||
-    possuiSimbolo == 0)
-{
-    Console.WriteLine("SENHA FRACA");
-}else Console.WriteLine("Senha FORTE!");
+var musicas = MusicasDoCsv(reader);
+ExibirMusicas(musicas);
 //Métodos
+void ExibirMusicas(IEnumerable<Musica> musicas)
+{
+    string colunaTitulo = "Titulo".PadRight(40);
+    string colunaArtista = "Artista".PadRight(20);
+    string colunaDuracao = "Duracao".PadRight(20);
+    string colunaGenero = "Genero".PadRight(20);
+    string colunaLancamento = "Lancamento".PadRight(20);
+    
+    Console.WriteLine("Musicas do Arquivo".PadLeft(53));
+    Console.WriteLine(colunaTitulo + colunaArtista + colunaDuracao + colunaGenero + colunaLancamento);
+    Console.WriteLine("".PadRight(120, '='));
+    
+    foreach (var musica in musicas.Take(5))
+    {
+        Console.WriteLine($"{musica.Titulo,-40}{musica.Artista,-20}{musica.Duracao/60.0,-20:F2}{musica.Generos[0],-20}{musica.Lancamento,-20:dd/MM/yyyy}");
+        Console.WriteLine("".PadRight(120, '-'));
+    }
+}
+
+void Senha()
+{
+    var senha = "Nicolas@26";
+
+    int totalCaracteres = senha.Length;// ou poderiamos usar o .Count()
+    int possuiLetrasMaiusculas = senha.Count(c => char.IsUpper(c)!);
+    int possuiLetrasMinusculas = senha.Count(c => char.IsLower(c)!);
+    int possuiNumero = senha.Count(c => char.IsDigit(c)!);
+    int possuiSimbolo = senha.Count(c => !char.IsLetterOrDigit(c));
+
+    if (totalCaracteres < 8 || possuiLetrasMaiusculas == 0 ||
+        possuiLetrasMinusculas == 0 || possuiNumero == 0 ||
+        possuiSimbolo == 0)
+    {
+        Console.WriteLine("SENHA FRACA");
+    }else Console.WriteLine("Senha FORTE!");
+}
+
 IEnumerable<Musica> MusicasDoCsv(StreamReader leitor)
 {
     var linha = leitor.ReadLine();
@@ -23,13 +48,14 @@ IEnumerable<Musica> MusicasDoCsv(StreamReader leitor)
     while (linha != null)
     {
         var partes = linha.Split(';');
-        if (partes.Length != 4) continue;
+        //if (partes.Length != 4) continue;
         var musica = new Musica
         {
             Titulo = partes[0],
             Artista = partes[1],
             Duracao = Convert.ToInt32(partes[2]),
-            Generos = partes[3].Split(',', StringSplitOptions.TrimEntries)//remove os espaços em branco indesejados
+            Generos = partes[3].Split(',', StringSplitOptions.TrimEntries),//remove os espaços em branco indesejados
+            Lancamento = Convert.ToDateTime(partes[4])
         };
         yield return musica;
         linha = leitor.ReadLine();
@@ -43,5 +69,6 @@ class Musica
     public string Titulo { get; set; }
     public string Artista { get; set; }
     public int Duracao { get; set; }
-    public string[] Generos { get; set; } = [];
+    public string[] Generos { get; set; }
+    public DateTime Lancamento { get; set; }
 }
