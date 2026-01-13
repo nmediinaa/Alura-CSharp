ef300d22af7dd9c9a47d58be2c185a8891185815
nmedina <nicolascostam23@gmail.com>
2026-01-13 00:01:45 -0300

    :sparkles: feat: Add some REGEX excercise

--- Praticando-ManipulacaoDeDados/Praticando-StringsERegex/Praticando-Regex/Program.cs
+++ Praticando-ManipulacaoDeDados/Praticando-StringsERegex/Praticando-Regex/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.RegularExpressions;
 
-Console.WriteLine("Hello, World!");
+//O valor total é R$ 99,90 e pode ser parcelado em 3x de R$ 33,30.
+void RetornandoUmValor()
+{
+    Console.WriteLine("Digite um código:");
+    string texto = Console.ReadLine()!;
+
+    Regex valueValidation = new Regex(@"^R\$\d{2}\,\d{2}$");
+    string valor = Regex.Match(texto, @"R\$ \d+,\d{2}").Value;
+    Console.WriteLine($"{valor}");
+}
+void ValidandoSomenteNumeros()
+{
+    Console.WriteLine("Digite um código:");
+    string codigo = Console.ReadLine()!;
+
+    Regex codeValidation = new Regex(@"^\d+$");
+
+    bool isValid = codeValidation.IsMatch(codigo);
+
+    if (isValid) Console.WriteLine("Codigo valido!");
+    else Console.WriteLine("Codigo invalido!");
+   
+}
