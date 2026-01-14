9822d50d16c5c7963da4caaf029d088dcf95c3fd
nmediinaa <nicolascostam23@gmail.com>
2026-01-14 18:16:35 -0300

    :sparkles: feat: Add Sanitizer class

---
+++ Praticando-ManipulacaoDeDados/ContactSanitizer/ContactSanitizer/ClienteSanitizer.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace ContactSanitizer;
+
+public class ClienteSanitizer
+{
+    public string[] ClienteValues { get; set; }
+    private List<Cliente> _listCliente = new ();
+    
+    //Converte uma cadeia de caracteres para uma convenção conhecida! Sempre antes de usar deixar a string em LowerCase
+    TextInfo textInfo = new CultureInfo("pt-BR").TextInfo; 
+    public List<Cliente> Sanitize()
+    {
+        string[] values = {};
+        
+        foreach (var linha in ClienteValues)
+        {
+            values = linha.Split(';');
+            
+            string nome = textInfo.ToTitleCase(values[1].ToLower());
+            string nomeFormatado = Regex.Replace(nome, @"\s{2,}", " ");
+            
+            string telefoneFormatado = values[2];
+            
+            Cliente cliente = new Cliente()
+            {
+                Id = int.TryParse(values[0], out int id) ? id : -1,
+                Nome = nomeFormatado,
+                Tel = telefoneFormatado,
+                Email = values[3],
+            };
+            _listCliente.Add(cliente);
+        }
+        
+        return _listCliente;
+    }
+}
