62b8a9ac5d776b2c185d2ed6e160dea1e46bd11d
nmediinaa <nicolascostam23@gmail.com>
2026-01-14 18:20:38 -0300

    :sparkles: feat: wire up FileReader and ClienteSanitizer services

--- Praticando-ManipulacaoDeDados/ContactSanitizer/ContactSanitizer/Program.cs
+++ Praticando-ManipulacaoDeDados/ContactSanitizer/ContactSanitizer/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Globalization;
+using ContactSanitizer;
 
-Console.WriteLine("Hello, World!");
+FileReader fileReader = new FileReader("Dados.txt");
+string[] linhas = fileReader.ReaderLines();
+
+ClienteSanitizer clienteSanitizer = new ClienteSanitizer()
+{
+    ClienteValues = linhas
+};
+
+var listaCliente = clienteSanitizer.Sanitize();
+
+foreach (var cliente in listaCliente)
+{
+    Console.WriteLine($" | [{cliente.Id}] {cliente.Nome} {cliente.Tel} {cliente.Email}");
+}
+
+
+
