f84ae4e8f900de89fc480356870bbcb8896bd9aa
nmediinaa <nicolascostam23@gmail.com>
2026-01-14 18:14:42 -0300

    :sparkles: feat: Add a FileReader class to read a csv file

---
+++ Praticando-ManipulacaoDeDados/ContactSanitizer/ContactSanitizer/FileReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Headers;
+
+namespace ContactSanitizer;
+
+public class FileReader
+{
+   
+    public string FilePath { get; set; }
+    
+    public FileReader(string filePath)
+    {
+        this.FilePath = filePath;
+    }
+
+
+    public string[] ReaderLines()
+    {
+        return File.ReadAllLines(FilePath);
+    }
+}
