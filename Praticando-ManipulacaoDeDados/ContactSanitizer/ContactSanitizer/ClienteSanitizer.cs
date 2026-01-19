9be8305405f912993db512b76c8126e418d4b5ff
nmediinaa <nicolascostam23@gmail.com>
2026-01-19 17:08:17 -0300

    :sparkles: feat: Add a tel validation with REGEX

--- Praticando-ManipulacaoDeDados/ContactSanitizer/ContactSanitizer/ClienteSanitizer.cs
+++ Praticando-ManipulacaoDeDados/ContactSanitizer/ContactSanitizer/ClienteSanitizer.cs
@@ -10,6 +10,7 @@ public class ClienteSanitizer
     
     //Converte uma cadeia de caracteres para uma convenção conhecida! Sempre antes de usar deixar a string em LowerCase
     TextInfo textInfo = new CultureInfo("pt-BR").TextInfo; 
+    
     public List<Cliente> Sanitize()
     {
         string[] values = {};
@@ -19,18 +20,23 @@ public class ClienteSanitizer
         foreach (var linha in ClienteValues)
         {
             values = linha.Split(';');
-            
             string nome = textInfo.ToTitleCase(values[1].ToLower());
             string nomeFormatado = Regex.Replace(nome, @"\s{2,}", " ");
             
             string email = values[3];
             string emailValid = emailValidation.IsMatch(email) ? email : "Email invalido!";
             
+            string telefoneFormatado = Regex.Replace(values[2], @"[^\d]", "");
+            if (telefoneFormatado.Length < 11) telefoneFormatado = "Invalido";
+            else telefoneFormatado = Regex.Replace(telefoneFormatado, @"(\d{2})(\d{5})(\d{4})", "($1) $2-$3");
+            
+            
+            
             Cliente cliente = new Cliente()
             {
                 Id = int.TryParse(values[0], out int id) ? id : -1,
                 Nome = nomeFormatado,
-                Tel = values[2],
+                Tel = telefoneFormatado,//telValid.Groups[0].Value,
                 Email = emailValid,
             };
             _listCliente.Add(cliente);
