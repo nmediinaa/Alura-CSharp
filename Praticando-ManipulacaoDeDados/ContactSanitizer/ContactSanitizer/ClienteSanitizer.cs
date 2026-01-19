9ae1373966ff3f5e8f64e82b0daa66491bedfe79
nmediinaa <nicolascostam23@gmail.com>
2026-01-19 19:19:48 -0300

    :recycle: refactor: Changing return from Sanitize method to return a Relatorio object and add method to validates inputs from CSV file like tel and email

--- Praticando-ManipulacaoDeDados/ContactSanitizer/ContactSanitizer/ClienteSanitizer.cs
+++ Praticando-ManipulacaoDeDados/ContactSanitizer/ContactSanitizer/ClienteSanitizer.cs
@@ -5,17 +5,16 @@ namespace ContactSanitizer;
 
 public class ClienteSanitizer
 {
-    public string[] ClienteValues { get; set; }
     private List<Cliente> _listCliente = new ();
+    private List<Cliente> _listClienteFail = new();
+    public RelatorioFinal RelatorioFinal = new ();
     
     //Converte uma cadeia de caracteres para uma convenção conhecida! Sempre antes de usar deixar a string em LowerCase
     TextInfo textInfo = new CultureInfo("pt-BR").TextInfo; 
     
-    public List<Cliente> Sanitize()
+    public RelatorioFinal Sanitize(string[] ClienteValues)
     {
         string[] values = {};
-        
-        Regex emailValidation = new Regex(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
         
         foreach (var linha in ClienteValues)
         {
@@ -23,25 +22,37 @@ public class ClienteSanitizer
             string nome = textInfo.ToTitleCase(values[1].ToLower());
             string nomeFormatado = Regex.Replace(nome, @"\s{2,}", " ");
             
-            string email = values[3];
-            string emailValid = emailValidation.IsMatch(email) ? email : "Email invalido!";
-            
-            string telefoneFormatado = Regex.Replace(values[2], @"[^\d]", "");
-            if (telefoneFormatado.Length < 11) telefoneFormatado = "Invalido";
-            else telefoneFormatado = Regex.Replace(telefoneFormatado, @"(\d{2})(\d{5})(\d{4})", "($1) $2-$3");
-            
+            string telefone = TelValidation(values[2]);
+            string email = EmailValidation(values[3]);
             
             
             Cliente cliente = new Cliente()
             {
                 Id = int.TryParse(values[0], out int id) ? id : -1,
                 Nome = nomeFormatado,
-                Tel = telefoneFormatado,//telValid.Groups[0].Value,
-                Email = emailValid,
+                Tel = telefone,
+                Email = email,
             };
-            _listCliente.Add(cliente);
+            if(telefone != "Invalido" && email != "Email invalido!") RelatorioFinal.AdicionarCliente(cliente);
+            else RelatorioFinal.AdicionarClienteErro(cliente);
         }
+
+        return RelatorioFinal;
+    }
+
+    private string EmailValidation(string email)
+    { 
+        Regex emailValidation = new Regex(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+        string emailValid = emailValidation.IsMatch(email) ? email :  "Email invalido!";
         
-        return _listCliente;
+        return emailValid;
+    }
+    
+    private string TelValidation(string tel)
+    {
+        string telefoneFormatado = Regex.Replace(tel, @"[^\d]", "");
+        if (telefoneFormatado.Length < 11) return "Invalido";
+        //Representa um grupo('()') no pattern do regex ($1) $2-$3
+        else return Regex.Replace(telefoneFormatado, @"(\d{2})(\d{5})(\d{4})", "($1) $2-$3");
     }
 }
