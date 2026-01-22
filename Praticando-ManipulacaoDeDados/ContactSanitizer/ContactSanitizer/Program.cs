1c341609dcecc59aca80a091afd7a300aac915f5
nmediinaa <nicolascostam23@gmail.com>
2026-01-22 17:31:05 -0300

    :sparkles: feat: Add my ReporterGenerator Lib to generates CSV reports

--- Praticando-ManipulacaoDeDados/ContactSanitizer/ContactSanitizer/Program.cs
+++ Praticando-ManipulacaoDeDados/ContactSanitizer/ContactSanitizer/Program.cs
@@ -2,6 +2,7 @@
 using ContactSanitizer;
 using ContactSanitizer.Services;
 using ContactSanitizer.View;
+using ReporterGenerator;
 
 FileReader fileReader = new FileReader("Dados.txt");
 string[] linhas = fileReader.ReaderLines();
@@ -10,6 +11,23 @@ ClienteSanitizer clienteSanitizer = new ClienteSanitizer();
 
 RelatorioFinal relatorio = clienteSanitizer.Sanitize(linhas);
 
+List<Dictionary<string, string>> reportData = new();
+foreach (var cliente in  relatorio.ListaClientes)
+{
+    var record = new Dictionary<string, string>
+    {
+        { "Nome", cliente.Nome },
+        { "Email", cliente.Email },
+        { "Telefone", cliente.Tel }
+    };
+    
+    reportData.Add(record);
+}
+
+IReporter reporterGenerator = new CSVReporterGenerator(reportData);
+
+Console.WriteLine(reporterGenerator.Generate());
+Console.WriteLine("");
 relatorio.ExibirRelatorio();
 
 
