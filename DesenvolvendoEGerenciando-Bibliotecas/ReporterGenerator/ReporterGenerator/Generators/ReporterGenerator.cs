3cb56ab497cf6f0a43c4a0dbd24833287817a9cd
nmediinaa <nicolascostam23@gmail.com>
2026-01-26 15:33:23 -0300

    :sparkles: feat: Add a ReporterGenerator base class to implements diferents output files

---
+++ DesenvolvendoEGerenciando-Bibliotecas/ReporterGenerator/ReporterGenerator/Generators/ReporterGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace ReporterGenerator;
+
+public abstract class ReporterGenerator : IReporter
+{
+    public List<Dictionary<string, string>> Input { get; set; }
+    
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Footer { get; set; }
+    
+    public string Generate()
+    {
+        throw new NotImplementedException();
+    }
+}
